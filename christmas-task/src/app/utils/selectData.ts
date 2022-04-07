884dc1ac48034eec35b6ed4f553f87e9fe6a64e7
Liubomyr Demianchuk <demianchukliubomyr@gmail.com>
2022-04-07 15:04:22 +0200

    refactor code

--- christmas-task/src/app/utils/selectData.ts
+++ christmas-task/src/app/utils/selectData.ts
@@ -2,5 +2,5 @@ export const selectData = [
   { value: 'sort-name-max', name: 'By name from «A» to «Z»' },
   { value: 'sort-name-min', name: 'By name from «Z» to «A»' },
   { value: 'sort-year-max', name: 'By purchase year by ascending' },
-  { value: 'sort-year-min', name: 'By purchase year by decline' },
+  { value: 'sort-year-min', name: 'By purchase year by descending' },
 ];
